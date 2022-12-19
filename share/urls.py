6fd8f0f394278a0201ed5f918007cb3ba2c93acc
bard <xx61509xx@gmail.com>
2022-12-19 17:00:05 +0800

    🚧 WIP: 分寶框架研究中

--- share/urls.py
+++ share/urls.py
@@ -2,5 +2,6 @@ from share import views
 from django.urls import include, path, re_path
 
 urlpatterns = [
-    re_path(r'group/$', views.Group.as_view()),
+    re_path(r'', views.Share.as_view()),
+    # re_path(r'group/(\S+)/$', views.GroupJoin.as_view()),
 ]
--- share/views.py
+++ share/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
+from django.http import HttpResponseRedirect
 # Create your views here.
 
-class Group(TemplateView):
+class Share(TemplateView):
     def get(self, request):
-        # if request.user.is_anonymous:
-        #     return HttpResponseRedirect('/accounts/login/')
-        return render(request, 'group.html')
+        if request.user.is_anonymous:
+            return HttpResponseRedirect('/accounts/login/')
+        return render(request, 'share_base.html')
