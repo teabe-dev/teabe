0364951612d4da7744b44fbe072d6a0b8c44ddfe
bard <xx61509xx@gmail.com>
2023-01-31 17:50:06 +0800

    ✨ feat: 可以顯示個人分寶群目錄

--- share/enums.py
+++ share/enums.py
@@ -1,6 +1,41 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 
-class MemberType(IntEnum):
+class StrChoiceEnum(str, Enum):
+
+    @classmethod
+    def nameList(cls):
+        return list(map(lambda x: x.name, cls))
+
+    @classmethod
+    def valueList(cls):
+        return list(map(lambda x: x.value, cls))
+
+    @classmethod
+    def choices(cls):
+        return tuple(map(lambda x: (x.value, x.name), cls))
+
+    @classmethod
+    def dictionary(cls):
+        return dict(map(lambda x: x, cls.choices()))
+
+class IntChoiceEnum(IntEnum):
+    @classmethod
+    def nameList(cls):
+        return list(map(lambda x: x.name, cls))
+
+    @classmethod
+    def valueList(cls):
+        return list(map(lambda x: x.value, cls))
+
+    @classmethod
+    def choices(cls):
+        return tuple(map(lambda x: (x.value, x.name), cls))
+
+    @classmethod
+    def dictionary(cls):
+        return dict(map(lambda x: x, cls.choices()))
+
+class MemberRole(IntChoiceEnum):
     INVITE = 11    # 邀請中
     AUDIT = 12     # 審核中
     BLOCKADE = 13  # 封鎖中
@@ -10,12 +45,13 @@ class MemberType(IntEnum):
 
     @classmethod
     def choices(cls):
-        CHOICES = (
-            (cls.INVITE.value, '邀請中'),
-            (cls.AUDIT.value, '審核中'),
-            (cls.BLOCKADE.value, '封鎖中'),
-            (cls.OWNER.value, '所有者'),
-            (cls.ADMIN.value, '管理員'),
-            (cls.MEMBER.value, '會員'),
-        )
-        return CHOICES
+        return tuple(map(lambda x: (x.value, MEMBER_ROLE_MAP[x]), cls))
+
+MEMBER_ROLE_MAP = {
+    MemberRole.INVITE: "邀請中",
+    MemberRole.AUDIT: "審核中",
+    MemberRole.BLOCKADE: "封鎖中",
+    MemberRole.OWNER: "所有者",
+    MemberRole.ADMIN: "管理員",
+    MemberRole.MEMBER: "會員",
+}
--- share/views.py
+++ share/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
-from django.views.generic import View, TemplateView
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
+
+from share.enums import MemberRole
 from share.forms import NewGroupForm
-from share.models import ShareMember
-from share.enums import MemberType
+from share.models import ShareGroup, ShareMember
+
 # Create your views here.
 
 class Share(TemplateView):
     def get(self, request):
         if request.user.is_anonymous:
             return HttpResponseRedirect('/accounts/login/')
-        return render(request, 'share_base.html')
+        share_data = ShareMember.objects.filter(user=request.user)
+        return render(request, 'share_base.html', {'share_data': share_data})
+
+
+class Information(TemplateView):
+    def get(self, request):
+        share_data = ShareMember.objects.filter(user=request.user)
+        return render(request, 'information.html', {'share_data': share_data})
 
 
 class NewGroup(TemplateView):
@@ -19,8 +28,10 @@ class NewGroup(TemplateView):
             return HttpResponseRedirect('/accounts/login/')
 
         form = NewGroupForm()
+        share_data = ShareMember.objects.filter(user=request.user)
         context = {
-            'form': form
+            'form': form,
+            'share_data': share_data
         }
         return render(request, 'new_group.html', context)
 
@@ -28,5 +39,14 @@ class NewGroup(TemplateView):
         form = NewGroupForm(request.POST)
         if form.is_valid():
             share_group = form.save()
-            ShareMember.objects.create(share_group=share_group, user=request.user, member_type=MemberType.OWNER)
-        return render(request, 'new_group.html', {'form': form})
+            ShareMember.objects.create(share_group=share_group, user=request.user, nick_name=request.user.username, member_type=MemberRole.OWNER)
+            # 轉跳
+
+        share_data = ShareMember.objects.filter(user=request.user)
+        return render(request, 'new_group.html', {'form': form, 'share_data': share_data})
+
+class Group(TemplateView):
+    def get(self, request, token):
+        share_data = ShareMember.objects.filter(user=request.user)
+        # TODO 顯示分寶資訊
+        return render(request, 'information.html', {'share_data': share_data})
